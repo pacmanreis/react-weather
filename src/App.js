c11fc11c89dd0cba4a5aa85d7378a1703a9e2d43
Paulo Reis <pacmanreis@gmail.com>
2019-10-04 19:52:13 +0100

    Added components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from './Components/Form'
+import Card from './Components/Card'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      loading: false,
+      loading: true,
       city: "Lisbon",
       geocode: "",
-      weatherInfo: null
+      weatherInfo: []
     }
     this.handleSearch = this.handleSearch.bind(this)
     this.handleChange = this.handleChange.bind(this)
@@ -17,15 +19,18 @@ class App extends Component {
   handleSearch() {
     this.setState({ loading: true })
     fetch(`https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${this.state.city}&format=json`)
-      .then(response => response.json())
-      .then(data => { this.setState({ geocode: data[0] }) })
-      .then(response => fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_KEY}/${this.state.geocode.lat},${this.state.geocode.lon}?units=si&lang=pt&exclude=hourly`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.error !== "Unable to geocode")
+      { this.setState({ geocode: data[0] })
+        fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_KEY}/${this.state.geocode.lat},${this.state.geocode.lon}?units=si&lang=pt&exclude=hourly`)
           .then(response => response.json())
           .then(data => this.setState({
             weatherInfo: data,
             loading: false
           }))
-      )
+      }
+    })
   }
 
   handleChange(event) {
@@ -33,23 +38,24 @@ class App extends Component {
     this.setState({
       [name]: event.target.value
     })
+    this.handleSearch()
   }
 
   render() {
+    let displayResults = false
+    if (this.state.weatherInfo !== undefined && this.state.loading === false) {
+      displayResults = this.state.weatherInfo.daily.data.map( (info) => <div className="card"><Card state={info} /></div>)
+    }
+
     return (
       <div>
-        <input name="city" value={this.state.city} onChange={this.handleChange} />
-        <button onClick={this.handleSearch}>search</button>
-        <p>City: {this.state.city}</p>
-        <p>Lat: {this.state.geocode.lat}</p>
-        <p>Lon: {this.state.geocode.lon}</p>
-        <p>{this.state.weatherInfo === null || this.state.loading === true ? "Loading..." : this.state.weatherInfo.currently.temperature }</p>
-        <p>{this.state.weatherInfo === null || this.state.loading === true ? "Loading..." : this.state.weatherInfo.daily.summary }</p>
-        <p>{this.state.weatherInfo === null || this.state.loading === true ? "Loading..." : this.state.weatherInfo.daily.data.map( temp => temp["temperatureHigh"]) }</p>
+        <Form name="city" handleChange={this.handleChange} data={this.state} />
+        <div className="card-list">
+          {displayResults}
+        </div>
       </div>
     );
   }
-
 }
 
 export default App;
