57912d37322004a897c12fc897bbec8d1bca77cc
Paulo Reis <pacmanreis@gmail.com>
2019-10-05 16:01:30 +0100

    Cards of the week now only show the next 7 days, unix timestamp now displays as date

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {Component} from 'react'
 
+class Card extends Component {
+  convert(unixTimestamp){
+   const weekDays = ['Sunday','Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday']
+   const date = new Date(unixTimestamp*1000)
+   const weekDay = weekDays[date.getDay()]
+   return weekDay
+  }
 
-
-function Card(props) {
-  return(
-    <div>
-      <p>High: {props.state.temperatureHigh}</p>
-      <p>Low: {props.state.temperatureLow}</p>
-    </div>
-  )
+  render() {
+    return(
+      <div>
+        <p>{this.convert(`${this.props.state.time}`)}</p>
+        <p>High: {this.props.state.temperatureHigh}</p>
+        <p>Low: {this.props.state.temperatureLow}</p>
+        <p>{this.props.state.summary}</p>
+      </div>
+    )
+  }
 }
 
 export default Card
